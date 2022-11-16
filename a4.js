8a2917a3ca21b953b1449b7b91b5271c660fa315
ombajad <118257639+ombajad@users.noreply.github.com>
2022-11-16 08:00:55 +0530

    Update a4.js

---
+++ a4.js
@@ -0,0 +1,17 @@
+npm init
+npm i express
+node app.js 
+
+
+const express = require('express')
+const app = express()
+const port = 5000
+app.use(express.static("public"));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+    res.sendFile("index.html");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
